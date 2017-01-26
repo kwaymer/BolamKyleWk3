8bd2c4f38d60bba7ee04fe7563aa47dc27901e5f
kylebolam <kyle_bolam@me.com>
2017-01-25 21:20:24 -0500

    Additions made to the app from Monday.
    Includes:
    List Items
    List Views
    Adapters
    Intents
    Contexts

--- app/src/main/java/com/example/kylebolam/bolamkylewk3/MainActivity.java
+++ app/src/main/java/com/example/kylebolam/bolamkylewk3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kylebolam.bolamkylewk3;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,9 +8,11 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton bt1;
+    private ImageButton bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private void initialView(){
 
         bt1 = (ImageButton) findViewById(R.id.button1);
+        bt3 = (ImageButton) findViewById(R.id.button3);
     }
 
     private void initialListener(){
@@ -30,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(v.getContext(), "Button 1 was clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ListViewActivity.class);
+                startActivity(intent);
             }
         });
     }
